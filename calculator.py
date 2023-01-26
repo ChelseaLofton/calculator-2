a07d73051b9f888e4994cf9a0276b60bb72ae8d9
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-01-25 17:29:04 -0800

    final commit

--- calculator.py
+++ calculator.py
@@ -4,7 +4,6 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
 while True:
     input_string = input("Please provide an operator and the numbers to calculate: ")
     tokens = input_string.split(' ')
@@ -12,14 +11,22 @@ while True:
     if "q" in tokens:
         print("You will exit the calculator.")
         break
+
+    if len(tokens) > 3 or len(tokens) < 2:
+        print("Please provide an operator and at least one number.")
+        continue
     
     operator = tokens[0]
     num1 = tokens[1]
-    
+
     if len(tokens) < 3:
         num2 = 0
     else:
         num2 = tokens[2]
+
+    if not num1.isdigit() or not num2.isdigit():
+        print("Those aren't numbers")
+        continue
     
     result = None
     
@@ -41,3 +48,5 @@ while True:
         result = mod(float(num1), float(num2))
 
     print(result)
+
+    
